4aa2046b884ef2d9c68fe0bf332be34b864d3a31
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-04-10 04:24:18 +0200

    Add example

--- src/test/java/de/eldoria/nashorn/Nashorn.java
+++ src/test/java/de/eldoria/nashorn/Nashorn.java
@@ -1,2 +1,15 @@
-package de.eldoria.nashorn;public class Nashorn {
+package de.eldoria.nashorn;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+public class Nashorn {
+    public void example() {
+        RegisteredServiceProvider<ScriptEngineManager> registration = Bukkit.getServer().getServicesManager().getRegistration(ScriptEngineManager.class);
+        ScriptEngineManager scriptEngineManager = registration.getProvider();
+        ScriptEngine nashorn = scriptEngineManager.getEngineByName("nashorn");
+    }
 }
